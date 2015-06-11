ed1a9fe6f61afecf782f0128071324e1f5257474
Frederic CORNU <fcornu@wardsback.org>
2015-06-11 16:05:01 +0200

    - renamed 'type' table to avoid clashes with SQL keywords - we don't DROP tables if they exist before main DDL script execution

--- messier-sql/main_DDL.sql
+++ messier-sql/main_DDL.sql
@@ -1,8 +1,6 @@
 --
 -- Table structure for table `constellation`
 --
-
-DROP TABLE IF EXISTS `constellation`;
 CREATE TABLE `constellation` (
   `con_id` char(3) COLLATE utf8_unicode_ci NOT NULL,
   `nomen` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
@@ -12,11 +10,9 @@ CREATE TABLE `constellation` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
--- Table structure for table `type`
+-- Table structure for table `objtype`
 --
-
-DROP TABLE IF EXISTS `type`;
-CREATE TABLE `type` (
+CREATE TABLE `objtype` (
   `type_id` varchar(10) COLLATE utf8_unicode_ci NOT NULL,
   `name` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
   `nom` varchar(45) COLLATE utf8_unicode_ci NOT NULL,
@@ -26,8 +22,6 @@ CREATE TABLE `type` (
 --
 -- Table structure for table `messier`
 --
-
-DROP TABLE IF EXISTS `messier`;
 CREATE TABLE `messier` (
   `id` tinyint(4) NOT NULL,
   `nom` varchar(45) COLLATE utf8_unicode_ci DEFAULT NULL,
@@ -42,6 +36,6 @@ CREATE TABLE `messier` (
   KEY `fk_messier_const` (`con_id`),
   KEY `fk_messier_type` (`type_id`),
   CONSTRAINT `fk_messier_const` FOREIGN KEY (`con_id`) REFERENCES `constellation` (`con_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
-  CONSTRAINT `fk_messier_type` FOREIGN KEY (`type_id`) REFERENCES `type` (`type_id`) ON DELETE NO ACTION ON UPDATE CASCADE
+  CONSTRAINT `fk_messier_type` FOREIGN KEY (`type_id`) REFERENCES `objtype` (`type_id`) ON DELETE NO ACTION ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
