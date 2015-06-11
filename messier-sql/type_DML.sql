38c4d9bdc921336c2d4c2a185a95ef1126533071
Frederic CORNU <fcornu@wardsback.org>
2015-06-11 15:44:43 +0200

    Population initiale

---
+++ messier-sql/type_DML.sql
@@ -0,0 +1,18 @@
+--
+-- Dumping data for table `type`
+--
+
+INSERT INTO `type` VALUES ('**','Double Star','Etoile double');
+INSERT INTO `type` VALUES ('Assoc*','Association of stars','Association d\'étoiles');
+INSERT INTO `type` VALUES ('Cl*','Cluster of Stars','Amas d\'étoiles');
+INSERT INTO `type` VALUES ('G-E','Elliptical galaxy','Galaxie elliptique');
+INSERT INTO `type` VALUES ('G-Irr','Irregular galaxy','Galaxie irrégulière');
+INSERT INTO `type` VALUES ('G-S','Spiral galaxy','Galaxie spirale');
+INSERT INTO `type` VALUES ('G-SB','Barred Spiral galaxy','Galaxy spirale barrée');
+INSERT INTO `type` VALUES ('GlCl','Globular Cluster','Amas globulaire');
+INSERT INTO `type` VALUES ('HII','HII (ionized) region','Région HII (ionisé)');
+INSERT INTO `type` VALUES ('OpCl','Open Cluster','Amas ouvert');
+INSERT INTO `type` VALUES ('PN','Planetary Nebula','Nébuleuse planétaire');
+INSERT INTO `type` VALUES ('RfNeb','Reflection nebula','Nébuleuse par réflexion');
+INSERT INTO `type` VALUES ('SNR','SuperNova Remnant','Rémanent de supernova');
+
